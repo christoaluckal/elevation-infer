be924ff46bed415106435a354082ae8150cba5db
christoaluckal <christo12aluckal@gmail.com>
2021-10-12 19:31:42 +0530

    contour check

--- image_process/segment.py
+++ image_process/segment.py
@@ -19,85 +19,44 @@ diag_range = (9360,6852,10030,8194)
 # diag_img = og_img[diag_range[0]:diag_range[2],diag_range[1]:diag_range[3]]
 
 
-area = dr.process_area('/home/caluckal/Desktop/Github/elevation-infer/DBCA_DEM.tif','/home/caluckal/Desktop/Github/elevation-infer/DBCA_DTM.tif',13492,5884,14988,8004)
-import numpy as np
-area = np.array(area)
-
-cv2.imwrite('bin_good.png',area)
-
-from PIL import Image
-import numpy as np
-
-formatted = (area * 255 / np.max(area)).astype('uint8')
-img = Image.fromarray(formatted)
-img.save('bin_good.png')
-
-image = cv2.imread("bin_good.png")
-
-# convert to RGB
-image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-# convert to grayscale
-gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-
-# create a binary thresholded image
-_, binary = cv2.threshold(gray, 1, 255, cv2.THRESH_BINARY_INV)
-# show it
-plt.imshow(binary, cmap="gray")
-plt.show()
-
-# find the contours from the thresholded image
-contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-# draw all contours
-
-
-# image = cv2.drawContours(image, contours, -1, (0, 255, 0), 2)
-sorted_contours= sorted(contours, key=cv2.contourArea, reverse= True)
-
-
-largest_item= sorted_contours[0]
-# print(largest_item)
-
-def get_contour_areas(contours):
+def get_contour_areas(contours,tot_pixel):
 
     all_areas= []
-
     for cnt in contours:
-        area= cv2.contourArea(cnt)
-        if area > 1000:
+        area = cv2.contourArea(cnt)
+        if area > tot_pixel*0.17 and area < tot_pixel*0.95:
+            print(area)
             all_areas.append(cnt)
 
     return all_areas
 
+def draw_contours(y1,x1,y2,x2):
+    area = dr.process_area('/home/caluckal/Desktop/Github/elevation-infer/DBCA_DEM.tif','/home/caluckal/Desktop/Github/elevation-infer/DBCA_DTM.tif',y1,x1,y2,x2)
+    import numpy as np
+    area = np.array(area)
+    from PIL import Image
+    import numpy as np
 
-# print(get_contour_areas(contours))
-image = cv2.drawContours(image, get_contour_areas(contours), -1, (0, 255, 0), 2)
+    formatted = (area * 255 / np.max(area)).astype('uint8')
+    img = Image.fromarray(formatted)
+    image = np.array(img)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    _, binary = cv2.threshold(gray, 1, 255, cv2.THRESH_BINARY_INV)
+    contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    image_pixel_count = image.shape[0]*image.shape[1]
+    # sorted_contours= sorted(contours, key=cv2.contourArea, reverse= True)
+    contour_list = get_contour_areas(contours,image_pixel_count)
+    # image = cv2.drawContours(image, get_contour_areas(contours), -1, (0, 255, 0), 2)
+    image = cv2.drawContours(image,contour_list , -1, (0, 255, 0), 2)
+    print(contour_list)
+    plt.imshow(image)
+    plt.show()
 
-plt.imshow(image)
-plt.show()
 
-# cv2.imshow('a',area)
-# cv2.waitKey(0)
+draw_contours(11289,12027,11814,12312)
+# draw_contours(11243,10689,11747,11220)
+# draw_contours(13492,5884,14988,8004)
+# draw_contours(13312,8164,13964,8884)
+# draw_contours(9360,6852,10030,8194)
 
-# area = dr.process_area('/home/caluckal/Desktop/Github/elevation-infer/DBCA_DEM.tif','/home/caluckal/Desktop/Github/elevation-infer/DBCA_DTM.tif',11243,10689,11747,11220)
-# area = np.array(area)
-
-# cv2.imshow('a',area)
-# cv2.waitKey(0)
-
-# area = dr.process_area('/home/caluckal/Desktop/Github/elevation-infer/DBCA_DEM.tif','/home/caluckal/Desktop/Github/elevation-infer/DBCA_DTM.tif',13492,5884,14988,8004)
-# area = np.array(area)
-
-# cv2.imshow('a',area)
-# cv2.waitKey(0)
-
-# area = dr.process_area('/home/caluckal/Desktop/Github/elevation-infer/DBCA_DEM.tif','/home/caluckal/Desktop/Github/elevation-infer/DBCA_DTM.tif',13312,8164,13964,8884)
-# area = np.array(area)
-
-# cv2.imshow('a',area)
-# cv2.waitKey(0)
-
-# area = dr.process_area('/home/caluckal/Desktop/Github/elevation-infer/DBCA_DEM.tif','/home/caluckal/Desktop/Github/elevation-infer/DBCA_DTM.tif',9360,6852,10030,8194)
-# area = np.array(area)
-
-# cv2.imshow('a',area)
-# cv2.waitKey(0)

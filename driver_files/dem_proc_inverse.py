88474f04af3accedf1c20a79b03ab6544eb67c61
christoaluckal <christo12aluckal@gmail.com>
2021-10-30 12:49:57 +0530

    added a inverse calculation improving execution time by almost 17%

---
+++ driver_files/dem_proc_inverse.py
@@ -0,0 +1,377 @@
+from osgeo import gdal
+import numpy as np
+import affine
+import sys
+import cv2
+from PIL import Image
+import matplotlib.pyplot as plt
+import selector
+from time import time
+sys.path.append('.')
+
+# This function takes an (X,Y) coordinate with the affine transform matrix and returns latitude and longitude
+def get_lon_lat(affine_transform,x_coord,y_coord):
+    '''
+    This function is responsible for converting the (X,Y) coordinate to a Longitude,Latitude pair
+
+    Params
+    affine_transform: An affine transform matrix to determine the Latitude and Longitude of a given pixel coordinate
+    x_coord,y_coord: Coordinates whose Longitude and Latitude need to be determined
+
+    Returns
+    lon,lat: Longitude and Latitude of the given (X,Y) coordinate
+    '''
+    lon, lat = affine_transform * (x_coord,y_coord)
+    # print("LAT LON:",lon,lat)
+    return lon,lat
+
+
+def reject_outliers(data, m=2):
+    '''
+    Function to remove values that lie outside the specified standard deviation
+
+    Params
+    data: 1D array with values
+
+    Returns
+    data: 1D array with invalid values removed
+    '''
+    return data[abs(data - np.mean(data)) < m * np.std(data)]
+
+# This is the inverse of getLonLat
+def get_xy(affine_transform,lat,lon):
+    inverse_transform = ~affine_transform
+    x_coord, y_coord = [ round(f) for f in inverse_transform * (lon, lat) ]
+    # print("GETXT:",x_coord,y_coord,lon,lat)
+    return (x_coord,y_coord)
+
+
+def process_dem_quantile(affine_transform,x_min,y_min,building_region,width,height,min_cutoff_percent,max_cutoff_percent):
+    '''
+    This function is responsible for processing an array of relative heights are find the optimum point
+
+    Params
+    affine_transform: An affine transform matrix to determine the Latitude and Longitude of a given pixel coordinate
+    x_min,y_min: Top-left coordinates of the building wrt the original Orthomosaic Image
+    building_region: Array with the relative heights of the building only
+    width,height: Width and Height of the building bounding box
+    min_cutoff_percent: Percentage value below which height values are invalid
+    max_cutoff_percent: Percentage value above which height values are invalid
+
+    Returns
+    max_height: Maximum height after filtering
+    (lon,lat): Longitude and Latitude of the location with max_height
+    '''
+
+    # 1D list to store all relative heights
+    height_vals = []
+
+    # Since its a python list, we convert it into a Numpy array
+    building_region = np.array(building_region)
+
+    for i in range(width):
+        for j in range(height):
+            # Add all heights in the list
+            height_vals.append(building_region[j][i])
+
+    # Remove values lying outside SD=2
+    height_vals = reject_outliers(np.array(sorted(height_vals)),2)
+
+    # Values that determine the percentile of heights
+    p1 = np.percentile(height_vals,min_cutoff_percent)
+    p2 = np.percentile(height_vals,max_cutoff_percent)
+
+    # Mask of values that lie between the specified percentiles
+    mask = ((height_vals > p1) & (height_vals<p2))
+
+    # height_vals is reduced in size to only contain valid heights
+    height_vals = height_vals[mask]
+
+    # Find the point with the maximum height
+    max_height = np.max(height_vals)
+
+    # Get index of the point with the maximum height
+    positions = np.where(building_region == max_height)
+
+    # Consider only the first point incase there are multiple
+    lon,lat = get_lon_lat(affine_transform,x_min+positions[1][0],y_min+positions[0][0])
+    return max_height,(lon,lat)
+    
+# Same as DEM, we process dtm to get the terrain height
+def process_dtm(dtm_file,x_min,y_min):
+    dtmdata = gdal.Open(str(dtm_file))
+    dtm_band = dtmdata.GetRasterBand(1)
+    dtm_area = dtm_band.ReadAsArray(x_min,y_min,1,1)
+    return dtm_area[0][0]
+
+
+def remove_inv_trees(sub_image,segmented_array):
+    '''
+    This function is responsible for processing the image and remove non-bulding entities such as trees
+
+    Params
+    sub_image: Image from which trees must be removed
+    segmented_array: Relative height array
+
+    Returns
+    mask: The same relative height array but with black pixel location removed
+    '''
+
+    # HSV value of the minimum green value
+    low_green = np.array([25, 20, 20])
+
+    # HSV value of the maximum green value
+    high_green = np.array([100, 255,255])
+
+    # greent_test = np.array([[[197,200,147]]],np.uint8)
+    # greent_test = cv2.cvtColor(greent_test,cv2.COLOR_BGR2HSV)
+
+    # Convert image to HSV
+    imgHSV = cv2.cvtColor(sub_image, cv2.COLOR_BGR2HSV)
+
+    #cv2.imwrite('image_hsv.jpg',imgHSV)
+
+    # create the Mask
+    mask = cv2.inRange(imgHSV, low_green, high_green)
+
+    #cv2.imwrite('init_mask.jpg',mask)
+
+    # invert the mask
+    mask = 255-mask
+
+    #cv2.imwrite('invert_mask.jpg',mask)
+
+    # Perform bitwise AND operation on image and mask to get the image without trees
+    mask = cv2.bitwise_and(sub_image, sub_image, mask=mask)
+    op = []
+    for height in range(0,len(mask)):
+        row = []
+        for width in range(0,len(mask[0])):
+            row.append(float(bool(mask[height][width][0] & mask[height][width][1] & mask[height][width][2]))*segmented_array[height][width])
+        op.append(row)
+
+    mask = np.array(op)
+    # cv2.imwrite('image_with_mask.jpg',mask)
+
+    return mask
+
+
+def get_contour_areas(contour_list,total_pixel_count,min_contour_area,max_contour_area):
+    '''
+    This function is responsible for processing the contour list to determine valid contours
+
+    Params
+    contour_list: List of every contour found in the image
+    total_pixel_count: Total number of pixels in the image. Metric of comparison
+    min_contour_area: Percentage of the area below which contours are invalid
+    max_contour_area: Percentage of the area above which contours are invalid
+
+    Returns
+    valid_contour_list: List of contours that are filtered and deemed to be valid
+    bounding_rectangle_params: A list of list where the inner list is a 1D array having the top-left coordinates of the bounding rectangle and the width and 
+    height of each rectangle: (x,y,w,h).
+    '''
+    valid_contour_list= []
+    bounding_rectangle_params = []
+    for cnt in contour_list:
+        area = cv2.contourArea(cnt)
+        if area > total_pixel_count*min_contour_area and area < total_pixel_count*max_contour_area:
+            x,y,w,h = cv2.boundingRect(cnt)
+            valid_contour_list.append(cnt)
+            bounding_rectangle_params.append([x,y,w,h])
+
+    return valid_contour_list,bounding_rectangle_params
+
+
+def process_inv_area(dem_file,dtm_file,y_min,x_min,y_max,x_max):
+    '''
+    This function is responsible for processing the DEM and DTM and create an array where invalid locations are black
+    and valid locations have values equal to their relative heights.
+
+    Params
+    dem_file: The path to the DEM file
+    dtm_file: The path to the DTM file
+    y_min,x_min: Top left coordinates of the ROI
+    y_max,x_max: Bottom right coordinates of the ROI
+    nongreens: Image with trees removed
+
+    Returns
+    area_diff: An array where valid locations have valid elevations and invalid locations are 0
+    '''
+
+    # Read the DEM file to get the DEM data
+    demdata = gdal.Open(str(dem_file))
+
+    # Since the DEM data may have other bands, we take only the height band
+    dem_band = demdata.GetRasterBand(1)
+
+    # The DEM band contains the entire DEM region, hence we extract only a specific location using the top-left coordinates and the width and height of the region
+    dem_area = dem_band.ReadAsArray(x_min,y_min,x_max-x_min,y_max-y_min)
+
+    # Delete the unnecessary value
+    del dem_band
+
+    # Repeat for DTM
+    dtmdata = gdal.Open(str(dtm_file))
+    dtm_band = dtmdata.GetRasterBand(1)
+    dtm_area = dtm_band.ReadAsArray(x_min,y_min,x_max-x_min,y_max-y_min)
+    del dtm_band
+
+    # This is a 2D array that holds the relative height of valid locations and 0 otherwise
+    area_diff = []
+    for height in range(y_max-y_min):
+        row_vals = []
+        for width in range(x_max-x_min):
+            # The condition is only true when it processes a [0 0 0] aka black pixel value. This is the invalid location
+            # if ~(nongreens[height][width][0] == 0 and nongreens[height][width][1] == 0 and nongreens[height][width][2] == 0):
+            height_val = dem_area[height][width]-dtm_area[height][width]
+            if height_val > 1:
+                # We append just the height value therefore this array when converted to image will have only intensity
+                # Meaning this array will become a grayscale image
+                row_vals.append(height_val)
+            else:
+                row_vals.append(0)
+            # else:
+            #     row_vals.append(0)
+        area_diff.append(row_vals)
+    return area_diff
+
+def draw_inv_contours(sub_image,dem_file,dtm_file,y_min,x_min,y_max,x_max,min_contour_area,max_contour_area):
+    '''
+    This function is responsible for processing the image to determine the number of buildings and the relative height of the buildings.
+
+    Params
+    sub_image: The small ROI image taken from the user
+    dem_file: The path to the DEM file
+    dtm_file: The path to the DTM file
+    y_min,x_min: Top left coordinates of the ROI
+    y_max,x_max: Bottom right coordinates of the ROI
+    min_contour_area: Percentage of the area below which contours are invalid
+    max_contour_area: Percentage of the area above which contours are invalid
+
+    Returns
+    len(contour_list): The number of buildings determined
+    contour_bounding_region: A list of list where the inner list is the relative height array of each bounding box region, therefore this list holds
+    2D arrays equal to the number of buildings
+    bounding_rectangle_params: A list of list where the inner list is a 1D array having the top-left coordinates of the bounding rectangle and the width and 
+    height of each rectangle: (x,y,w,h). Therefore bounding_rectangle_params[i] are the parameters of contour_bounding_region[i]
+    sub_image_contour: The sub-image with contours and bounding rectangles overlayed
+    '''
+
+    
+    # We generate a 2D array with values either as the relative height or 0
+    segmented_height_array = process_inv_area(dem_file,dtm_file,y_min,x_min,y_max,x_max)
+
+    # Here, we pass the relative height array and get the same array. Instead of receiving every pixel, the following
+    # function determines valid locations and returns the relative height of only those
+    segmented_height_array = remove_inv_trees(sub_image,segmented_height_array)
+
+    # Convert array to Numpy array
+    segmented_height_array = np.array(segmented_height_array)
+
+    # Normalize the array to hold integer values. We scale it up to 255 to process the array as an integer image
+    segmented_height_int_array = (segmented_height_array * 255 / np.max(segmented_height_array)).astype('uint8')
+
+    #cv2.imwrite('formatted.jpg',segmented_height_int_array)
+    
+    # Make the image into RGB
+    segmented_height_int_image = cv2.cvtColor(segmented_height_int_array, cv2.COLOR_BGR2RGB)
+
+    # Save the image to another array before further processing
+    image_rgb = cv2.cvtColor(sub_image,cv2.COLOR_BGR2RGB)
+
+    # The contour algorithm works on binarized iamges, we first convert the image to a grayscaled image.
+    gray = cv2.cvtColor(segmented_height_int_image, cv2.COLOR_RGB2GRAY)
+
+    #cv2.imwrite('gray_scaled.jpg',gray)
+
+    # Use thresholding to generate a binarized image where every pixel having intensity > 1 are ceiled to 255 or white
+    _, binary = cv2.threshold(gray, 1, 255, cv2.THRESH_BINARY_INV)
+    #cv2.imwrite('binarized.jpg',binary)
+
+    # Detect contours in the binarized image
+    contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Get the total number of pixels in the image as a metric of comparison
+    image_pixel_count = segmented_height_int_image.shape[0]*segmented_height_int_image.shape[1]
+
+    # Sort the contours according to their areas
+    sorted_contours= sorted(contours, key=cv2.contourArea, reverse= True)
+
+    # Process the contour list to determine the valid contours
+    contour_list,bounding_rectangle_params = get_contour_areas(sorted_contours,image_pixel_count,min_contour_area,max_contour_area)
+
+    # Draw contours on the original sub-image
+    sub_image_contour = cv2.drawContours(image_rgb,contour_list , -1, (255, 0, 0), 2)
+    #cv2.imwrite('contour_binary.jpg',cv2.drawContours(gray,contour_list , -1, (255, 0, 0), 2))
+
+    # Draw the bounding rectangles on the sub-image and save the valid regions to the array
+    contour_bounding_region = []
+    for rectangle_params in bounding_rectangle_params:
+        top_left_X,top_left_y = rectangle_params[0],rectangle_params[1]
+        rect_width,rect_height = rectangle_params[2],rectangle_params[3]
+        cv2.rectangle(sub_image_contour,(top_left_X,top_left_y),(top_left_X+rect_width,top_left_y+rect_height),(255,0,0),2)
+        contour_bounding_region.append(segmented_height_array[top_left_y:top_left_y+rect_height,top_left_X:top_left_X+rect_width])
+    # plt.imshow(sub_image_contour)
+    # plt.show()
+    #cv2.imwrite('sub_image_invalid_contour.jpg',cv2.cvtColor(sub_image_contour,cv2.COLOR_BGR2RGB))
+
+    return len(contour_list),contour_bounding_region,bounding_rectangle_params,sub_image_contour
+
+def process_model(original_ortho,dem_file,dtm_file,bounding_list,min_contour_area,max_contour_area,min_cutoff_percent,max_cutoff_percent):
+    '''
+    This function takes the DEM,DTM, point list and returns a dictionary with the (X1,Y1)(X2,Y2) of each building as key and (Lat,Lon),Relative height as values
+    Params
+    original_ortho: Original Orthomosaic Image array
+    dem_file: The path to the DEM file
+    dtm_file: The path to the DTM file
+    bounding_list: List containing the coordinates of each ROI
+    y_max,x_max: Bottom right coordinates of the ROI
+    min_contour_area: Percentage of the area below which contours are invalid
+    max_contour_area: Percentage of the area above which contours are invalid
+    min_cutoff_percent: Percentage value below which height values are invalid
+    max_cutoff_percent: Percentage value above which height values are invalid
+
+    Returns
+    loc_data: A dictionary holding the coordinates of each building as key along with their longitude,latitude and relative height
+    '''
+
+    # Convert the input parameters to percentile values
+    min_contour_area = float(min_contour_area/100)
+    max_contour_area = float(max_contour_area/100)
+
+    # Dictionary to save the value
+    loc_data = {}
+
+    # Open the DEM file to get the attributes 
+    demdata = gdal.Open(str(dem_file))
+
+    # Retrieve the affine transform matrix
+    dem_affine_transform = affine.Affine.from_gdal(*demdata.GetGeoTransform())
+
+    # Iterate through all the ROIs
+    for points in bounding_list:
+
+        # Coordinates of the ROI
+        x_min,y_min,x_max,y_max = int(points[0]),int(points[1]),int(points[2]),int(points[3])
+        size = ((y_max-y_min)*(x_max-x_min))
+        # Image of the ROI
+        sub_image = original_ortho[y_min:y_max,x_min:x_max]
+        #cv2.imwrite('small_image.jpg',sub_image)
+
+        # Get the number of buildings, contour list, bounding rectangles and the image with contour overlay
+        contour_count,contour_rectangle_region,points_list,image_rgb = draw_inv_contours(sub_image,dem_file,dtm_file,y_min,x_min,y_max,x_max,min_contour_area,max_contour_area)
+
+        # Select the buildings whose elevation needs to be found
+        selected_coords = selector.selector(image_rgb)
+        for contours_num in range(contour_count):
+            for coords in selected_coords:
+                x,y,w,h = points_list[contours_num]
+                # If the clicked building lies inside a bounding rectangle then process the building region
+                if coords[0] > x and coords[0] < x+w and coords[1] > y and coords[1] < y+h:
+                    dem_height,lon_lat = process_dem_quantile(dem_affine_transform,x_min+x,y_min+y,contour_rectangle_region[contours_num],w,h,min_cutoff_percent,max_cutoff_percent)
+                    loc_data["{},{},{},{}".format(x_min+x,y_min+y,x_min+w,y_min+h)] = [lon_lat,dem_height]
+        
+    return loc_data
+
+
---
+++ driver_files/inverse.py
@@ -0,0 +1,177 @@
+'''
+When this code runs, it checks for the input files. If the files are present then it asks the user to drag and select the regions 
+where processing needs to take place. Once all the regions are marked, press ESC and the code will run. Once processing is done another window
+is displayed to the user. This window shows the buildings present. The user then clicks on the buildings whose heights need to be determined and
+then press ESC. The output of the code shows a dictionary with the coordinates of buildings as keys and the Lat-Lon and relative height of the buildings present
+'''
+import cv2
+from matplotlib.pyplot import contour
+import dem_proc,dem_proc_inverse
+import sys
+
+# Invocation is python3 driver_point.py <JPG for clicking> <DEM file> <DTM file>
+args = sys.argv[1:]
+
+if len(args) < 3:
+    print("Missing files")
+    exit()
+else:
+    ortho_file = args[0]
+    dem_file = args[1]
+    dtm_file = args[2]
+    try:
+        contour_flag = args[3]
+        if contour_flag == "custom":
+            min_contour_area = float(input('Enter the min. contour area percentage:'))
+            max_contour_area = float(input('Enter the max. contour area percentage:'))
+            min_cutoff_percent = float(input('Enter the min quantile cutoff:'))
+            max_cutoff_percent = float(input('Enter the max. quantile cutoff:'))
+        else:
+            raise Exception
+    except:
+        print("Keeping default")
+        min_contour_area = 0.6
+        max_contour_area = 95
+        min_cutoff_percent = 25
+        max_cutoff_percent = 95
+        
+
+original_ortho_array = cv2.imread(ortho_file)
+ortho_shape = original_ortho_array.shape
+
+# We downscale the original image to be able to show it in a window. This leads to a ~5% error in pixel calculations
+downscaled_ortho = cv2.resize(original_ortho_array,(ortho_shape[1]//16,ortho_shape[0]//16))
+
+# Dummy image array for saving purposes
+dummy_img = original_ortho_array
+
+# Pixel location variables
+ix = -1
+iy = -1
+fx = -1
+fy = -1
+
+# Pixel location storage
+box_list = []
+
+
+ix = -1
+iy = -1
+fx = -1
+fy = -1
+drawing = False
+
+box_list = []
+loc_data = {}
+
+# def write_on_image(image,loc_data):
+#     for points,vals in loc_data.items():
+#         font = cv2.FONT_HERSHEY_SIMPLEX
+#         cv2.putText(image, str(vals[-1]), ((ix+fx)//2,(iy+fy)//2), font, 3, (0, 255, 0), 2, cv2.LINE_AA)
+#         # print(ix,iy,fx,fy,(vals[-3],vals[-2]))
+#         cv2.circle(image, (vals[-3],vals[-2]), 10, (255,0,0),thickness=-1)
+#     cv2.imwrite("DBCA_marked.jpg",image)
+
+
+# Since we need a precise location on the downscaled image, we normalize the pixel location using the downscaled resolution
+def normalizebb(box_list_val,shape):
+    '''
+    Function to normalize the coordinates
+
+    Params
+    box_list_val: 2D array with each element having the top-left and bottom-right coordinates of the downscaled ROI
+    shape: The value bounds that will be used for normalization
+
+    Returns
+    norm_box_list: 2D array with each element being normalized
+    '''
+    norm_box_list = []
+    for x in box_list_val:
+        # print(x,shape)
+        norm_box_list.append((x[0]/shape[1],x[1]/shape[0],x[2]/shape[1],x[3]/shape[0]))
+    
+    return norm_box_list
+
+# When we process the DEM we need the actual pixel locations and not the downscaled one. So we reverse the normalization using the original image resolution
+def reversenomarlize(box_list_val,shape):
+    '''
+    Function to upscale the coordinates to match the original Orthomosaic resolution
+
+    Params
+    box_list_val: 2D array with each element having the top-left and bottom-right coordinates of the downscaled ROI
+    shape: The resolution to which the values need to be upscaled
+
+    Returns
+    upscaled_box_list: 2D array with each element having the top-left and bottom-right coordinates of the upscaled ROI
+    '''
+    upscaled_box_list = []
+    for x in box_list_val:
+        # print(x,shape)
+        x1 = int(x[0]*shape[1])
+        y1 = int(x[1]*shape[0])
+        x2 = int(x[2]*shape[1])
+        y2 = int(x[3]*shape[0])
+        upscaled_box_list.append((x1,y1,x2,y2))
+    
+    return upscaled_box_list
+
+'''
+This function is called which lets the user define the ROI for further processing. Simply click the top-left of the ROI and then hold and drag
+the mouse to the bottom-right of the ROI.
+'''
+def draw_rectangle_with_drag(event, x, y, flags, param):
+      
+    global ix, iy, drawing, downscaled_ortho,fx,fy
+      
+    if event == cv2.EVENT_LBUTTONDOWN:
+        drawing = True
+        ix = x
+        iy = y            
+
+    elif event == cv2.EVENT_MOUSEMOVE:
+        fx = x
+        fy = y
+
+    elif event == cv2.EVENT_MOUSEWHEEL:
+        pass
+
+    elif event == cv2.EVENT_MBUTTONDOWN:
+        pass
+            
+    else:
+        cv2.line(downscaled_ortho, pt1 =(ix, iy),
+                          pt2 =(ix, fy),
+                          color =(0, 255, 255),
+                          thickness =1)
+        cv2.line(downscaled_ortho, pt1 =(ix, iy),
+                          pt2 =(fx, iy),
+                          color =(0, 255, 255),
+                          thickness =1)
+        cv2.line(downscaled_ortho, pt1 =(ix, fy),
+                          pt2 =(fx, fy),
+                          color =(0, 255, 255),
+                          thickness =1)
+        cv2.line(downscaled_ortho, pt1 =(fx, iy),
+                          pt2 =(fx, fy),
+                          color =(0, 255, 255),
+                          thickness =1)
+
+        box_list.append((ix,iy,fx,fy))
+
+          
+cv2.namedWindow(winname = "Downscaled Orthomosaic")
+cv2.setMouseCallback("Downscaled Orthomosaic", 
+                     draw_rectangle_with_drag)
+while True:
+    cv2.imshow("Downscaled Orthomosaic", downscaled_ortho)
+      
+    if cv2.waitKey(10) == 27:
+        normalized = normalizebb(box_list,downscaled_ortho.shape)
+        upscaled_coords = reversenomarlize(normalized,original_ortho_array.shape)
+        loc_data = dem_proc_inverse.process_model(original_ortho_array,dem_file,dtm_file,upscaled_coords,min_contour_area,max_contour_area,min_cutoff_percent,max_cutoff_percent)
+        print("\nElevations are\n")
+        for x,y in loc_data.items():
+            print('Coords:',x,'|\tValue:',y)
+        break
+  
+cv2.destroyAllWindows()
